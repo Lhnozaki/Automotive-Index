b9576c11eb2bba051ae3badb3de7346035f5cfbf
Lewis Nozaki <lhnozaki@gmail.com>
2019-08-31 11:13:25 -1000

    completed queries

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -1,12 +1,26 @@
 -- db indexed_cars
+DROP TABLE
+IF EXISTS car_models;
 
-CREATE TABLE IF NOT EXISTS car_models
+CREATE TABLE
+IF NOT EXISTS car_models
 (
  id serial,
- make_code character varying(125) NOT NULL,
- make_title character varying(125) NOT NULL,
- model_code character varying(125) NOT NULL,
- model_title character varying(125) NOT NULL,
+ make_code character varying
+(125) NOT NULL,
+ make_title character varying
+(125) NOT NULL,
+ model_code character varying
+(125) NOT NULL,
+ model_title character varying
+(125) NOT NULL,
  year integer NOT NULL,
- PRIMARY KEY (id)
-)
+ PRIMARY KEY
+(id)
+);
+
+CREATE INDEX codeIndex
+ON car_models (make_code, model_code);
+
+CREATE INDEX titleIndex
+ON car_models (make_title, model_title);
